7fb963e67d410dc287ddb29d446526ac7e3a26e1
WilliamVSan <williamvsantos75@gmail.com>
2022-04-25 21:41:59 -0300

    Atualizando repositório

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://i.imgur.com/vQOOXcT.gif" alt="olhando para cima" className='img' />
+    </>
+  );
+}
+
+export default Home;

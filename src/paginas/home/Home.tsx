f2f199ed75bddf8dbdd2fa02568a2b6301a825f8
WilliamVSan <williamvsantos75@gmail.com>
2022-04-27 21:03:12 -0300

    Personalizando Blog Pessoal.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,28 +1,64 @@
-import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
-import './Home.css';
+import React from "react";
+import { Typography, Box, Grid, Button } from "@material-ui/core";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-        <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
-                </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
-                </Grid>
-            </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: "#3F51B5" }}
+        className="body"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              style={{ color: "white", fontWeight: "bold",fontFamily: "Minecraft"}}
+            >
+              Seja Bem Vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              style={{ color: "white", fontWeight: "bold" }}
+            >
+              expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}></Box>
+            <Button
+              variant="outlined"
+              color="secondary"
+              style={{
+                borderColor: "black",
+                backgroundColor: "white",
+                color: "black",
+                fontWeight: "bold"
+              }}
+            >
+              Ver Postagens
+            </Button>
+          </Box>
+          <Box>
+          <img className="img" src="https://cdn.streamelements.com/uploads/b89b1bdb-0499-4760-a118-5b961d55c18c.gif" alt="gatinho" />
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+        <Grid xs={12} style={{ backgroundColor: "white" }}></Grid>
+      </Grid>
     </>
   );
 }

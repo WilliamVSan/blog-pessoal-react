1fa2442c95b0aa4c83ebc764361b81b34ea28dd1
William <86013044+WilliamVSan@users.noreply.github.com>
2022-05-03 15:16:37 -0300

    Update ListPost.tsx

--- src/components/Posts/listpost/ListPost.tsx
+++ src/components/Posts/listpost/ListPost.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListPost.css';
 
-function ListPost() {
+function ListaPostagem() {
 
   return (
     <>
